8aef27d77bd5b92805bb06c04f4efb1edea89243
Eric Martel <emartel@gmail.com>
2015-12-14 07:26:57 -0500

    Update namespace
    Added description of methods to add

--- src/email.php
+++ src/email.php
@@ -1,8 +1,17 @@
 <?php
 
-namespace Codeception\Module;
+namespace Codeception\Email;
 
 use Codeception\Module;
+
+// scan all emails for a specific subject
+// scan all emails for a part of a subject
+// scan all emails for a specific body
+// scan all emails for a part of a body
+// text & html content
+// validate sender
+// validate receivers (TO, CC, BCC)
+// validate attachments
 
 trait TestsEmails
 {
